09eca1064d383ed8b019739737f198608824d64c
DECIO SANTANA DE AGUIAR <88034016+Decioagu@users.noreply.github.com>
2022-05-20 19:23:34 -0300

    Adição de comentário

--- ex013/script.js
+++ ex013/script.js
@@ -7,12 +7,15 @@ function calcular(){
     } else {
         const n = parseInt(num.value)
         
+         // (etapa 4 - Opcional) limpa Tag "option" caso hajá texto pre existente
         res.innerHTML = ''
         for (var cont = 1; cont <= 10; cont++){
-            let item = document.createElement('option'); // cria "novo elemento" pelo JavaScript no HTML
+            // (etapa=1) cria "novo elemento" pelo JavaScript no HTML na tag "option"
+            let item = document.createElement('option');
+            // (etapa 2) adiciona valor  
             item.innerHTML = `${cont} x ${n} = ${cont*n}`; // cálculo a ser exibido
             /* item.value = `${cont}` */
-            // exibição do "novo elemento"
+            // (etapa 3) exibir novo elemento
             res.appendChild(item).style.textAlign = "left"; // (.style.textAlign = "left") o texto fica alinhado a esquerda
         }
     }
